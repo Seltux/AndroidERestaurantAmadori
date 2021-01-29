592a4daef2be7b9dc600cc24fa326dbb70835e20
Seltux <matteo.amadori@isen.yncrea.fr>
2021-01-29 09:20:23 +0100

    Add Price in function of Quantity

--- app/src/main/java/fr/isen/amadori/androiderestaurant/DetailsActivity.kt
+++ app/src/main/java/fr/isen/amadori/androiderestaurant/DetailsActivity.kt
@@ -1,5 +1,6 @@
 package fr.isen.amadori.androiderestaurant
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.squareup.picasso.Picasso
@@ -9,16 +10,26 @@ import fr.isen.amadori.androiderestaurant.databinding.ActivityDetailsBinding
 import fr.isen.amadori.androiderestaurant.model.Dish
 
 
-private lateinit var binding:ActivityDetailsBinding
+private lateinit var binding: ActivityDetailsBinding
 
 class DetailsActivity : AppCompatActivity() {
 
-    var quantity = 0
+    var quantity = 1
 
+
+    @SuppressLint("SetTextI18n")
     fun setText() {
         binding.idQuantity.setText(quantity.toString() + "")
+
     }
 
+    @SuppressLint("SetTextI18n")
+    fun setPrice(dishInfo: Dish) {
+        val price_quantity: Double = quantity.toDouble() * dishInfo.getPrice()
+        binding.idPriceRepasDetails.text = price_quantity.toString() + "€"
+    }
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailsBinding.inflate(layoutInflater)
@@ -38,27 +49,33 @@ class DetailsActivity : AppCompatActivity() {
                 R.drawable.pizza
             )
             val imageListener =
-                ImageListener { position, imageView -> Picasso.get().load(dishInfo.getFirstImage()).into(
-                    imageView
-                ) }
+                ImageListener { position, imageView ->
+                    Picasso.get().load(dishInfo.getFirstImage()).into(
+                        imageView
+                    )
+                }
             binding.carouselView.pageCount = sampleImages.size
             binding.carouselView.setImageListener(imageListener)
         }
         if (dishInfo != null) {
             binding.idNomRepasDetails.text = dishInfo.title
-        }
-        if (dishInfo != null) {
             binding.idPriceRepasDetails.text = dishInfo.getFormattedPrice()
         }
         binding.idFloatButtonPlus.setOnClickListener {
             quantity++
             setText()
+            if (dishInfo != null) {
+                setPrice(dishInfo)
+            }
         }
         binding.idFloatButtonMinus.setOnClickListener {
-            if(quantity>1) {
+            if (quantity > 1) {
                 quantity--
             }
             setText()
+            if (dishInfo != null) {
+                setPrice(dishInfo)
+            }
         }
 
     }
